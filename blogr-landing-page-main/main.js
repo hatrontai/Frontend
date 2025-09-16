40f9e06132a06d3137ceca5573682a16b72f3e3b
UIT-21520436 <21520436@gm.uit.edu.vn>
2025-09-17 00:06:40 +0700

    design mobile web blogr

--- blogr-landing-page-main/main.js
+++ blogr-landing-page-main/main.js
@@ -14,3 +14,27 @@ items.forEach(function(item, index) {
         }
     })
 })
+
+var signUp = document.querySelector('.navbar .account .sign-up')
+signUp.addEventListener("mouseover", function() {
+    signUp.querySelector('p').style.color = 'rgba(221, 218, 218, 0.901)';
+})
+signUp.addEventListener("mouseout", function() {
+    signUp.querySelector('p').style.color = '#c41a1ade';
+})
+
+var btnFre = document.querySelector('.header .premium .free')
+btnFre.addEventListener("mouseover", function() {
+    btnFre.querySelector('a').style.color = 'rgba(221, 218, 218, 0.901)';
+})
+btnFre.addEventListener("mouseout", function() {
+    btnFre.querySelector('a').style.color = '#c41a1ade';
+})
+
+var btnBuy = document.querySelector('.header .premium .buy')
+btnBuy.addEventListener("mouseout", function() {
+    btnBuy.querySelector('a').style.color = 'rgba(221, 218, 218, 0.901)';
+})
+btnBuy.addEventListener("mouseover", function() {
+    btnBuy.querySelector('a').style.color = '#c41a1ade';
+})
