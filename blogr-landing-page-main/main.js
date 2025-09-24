b2304ea8d59d2d530443d088c97e3b21967c6134
UIT-21520436 <21520436@gm.uit.edu.vn>
2025-09-24 23:35:08 +0700

    update task form

--- blogr-landing-page-main/main.js
+++ blogr-landing-page-main/main.js
@@ -1,3 +1,12 @@
+function a_colortoWhite() {
+    this.querySelector('a').style.color = 'rgba(221, 218, 218, 0.901)';
+}
+
+function a_colortoRed() {
+    this.querySelector('a').style.color = '#c41a1ade';
+}
+
+
 var items = document.querySelectorAll('.navbar .navbar-items .item')
 
 items.forEach(function(item, index) {
@@ -16,25 +25,51 @@ items.forEach(function(item, index) {
 })
 
 var signUp = document.querySelector('.navbar .account .sign-up')
-signUp.addEventListener("mouseover", function() {
-    signUp.querySelector('p').style.color = 'rgba(221, 218, 218, 0.901)';
-})
-signUp.addEventListener("mouseout", function() {
-    signUp.querySelector('p').style.color = '#c41a1ade';
-})
+signUp.addEventListener("mouseover", a_colortoWhite)
+signUp.addEventListener("mouseout", a_colortoRed)
 
 var btnFre = document.querySelector('.header .premium .free')
-btnFre.addEventListener("mouseover", function() {
-    btnFre.querySelector('a').style.color = 'rgba(221, 218, 218, 0.901)';
-})
-btnFre.addEventListener("mouseout", function() {
-    btnFre.querySelector('a').style.color = '#c41a1ade';
-})
+btnFre.addEventListener("mouseover", a_colortoWhite)
+btnFre.addEventListener("mouseout", a_colortoRed)
 
 var btnBuy = document.querySelector('.header .premium .buy')
-btnBuy.addEventListener("mouseout", function() {
-    btnBuy.querySelector('a').style.color = 'rgba(221, 218, 218, 0.901)';
-})
-btnBuy.addEventListener("mouseover", function() {
-    btnBuy.querySelector('a').style.color = '#c41a1ade';
+btnBuy.addEventListener("mouseout", a_colortoWhite)
+btnBuy.addEventListener("mouseover", a_colortoRed)
+
+var menu = document.querySelector('.navbar .menu')
+var menuCheck = false
+menu.onclick = function() {
+    menubar = this.querySelector('.menu-wrapper')
+    menubar.addEventListener('click', function(e) {
+        e.stopPropagation();
+    })
+    if (menuCheck === false) {
+        menuCheck = true;
+        this.style.backgroundImage = 'url("./images/icon-close.svg")';
+        menubar.style.display = 'flex';
+    }
+    else {
+        menuCheck = false
+        this.style.backgroundImage = 'url("./images/icon-hamburger.svg")'
+        menubar.style.display = 'none'
+    }
+}
+
+var menuItems = menu.querySelectorAll('.item')
+menuItems.forEach(function(item, index) {
+    var list = item.querySelector('.item-components')
+    if (list !== null) {
+        item.onclick = function() {
+            if (list.style.display === 'none' || list.style.display === "") {
+                list.style.display = 'flex'
+                item.querySelector('p').classList.add('rotate');
+                item.querySelector('p').style.color = '#403f3fa0'
+            }
+            else {
+                list.style.display = 'none'
+                item.querySelector('p').classList.remove('rotate');
+                item.querySelector('p').style.color = '#333'
+            }
+        }
+    }
 })
