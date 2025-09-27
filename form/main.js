d70aa4c7140a8fb67144b116bf2293a05408b84b
UIT-21520436 <21520436@gm.uit.edu.vn>
2025-09-27 23:34:04 +0700

    event submit form

--- form/main.js
+++ form/main.js
@@ -4,30 +4,71 @@ function Validator(options) {
 
     var selectorRules = {};
 
-    function check(inputElement, rule) {
-        var errorMessage = inputElement.parentElement.querySelector(options.formMessage) 
+    function getParrent(element, selector) {
+        while (element.parentElement) {
+            if(element.parentElement.matches(selector)) {
+                return element.parentElement
+            }
+            element = element.parentElement
+        }
+    }
+
+    // validate the input with rule
+    function validate(inputElement, rule) {
+        var errorMessage = getParrent(inputElement, options.formGroupSelector).querySelector(options.formMessage)
+        // var errorMessage = inputElement.parentElement.querySelector(options.formMessage) 
         var rules = selectorRules[rule.selector]
         for (i= 0; i < rules.length; i++) {
             if (rules[i].test(inputElement.value)) {
                 errorMessage.innerText = ""
-                inputElement.parentElement.classList.remove('invalid')
+                getParrent(inputElement, options.formGroupSelector).classList.remove('invalid')
             }
             else {
                 errorMessage.innerText = rules[i].errorMessage
-                inputElement.parentElement.classList.add('invalid')
-                break
+                getParrent(inputElement, options.formGroupSelector).classList.add('invalid')
+                return false
             }
         }
+        return true
     }
 
     var formElement = document.querySelector(options.form);
     if (formElement) {
+        
+        // event submit
+        formElement.onsubmit = function(e) {
+            e.preventDefault();
+
+            var isFormValid = true
+            // check value input
+            options.rules.forEach(function(rule) {
+                var inputElement = formElement.querySelector(rule.selector);
+                if (inputElement) {
+                    if (!validate(inputElement, rule)) {
+                        isFormValid = false
+                    }
+                }
+            })
+            if (isFormValid) {
+                if (typeof options.onSubmit === 'function') {
+
+                    var enableInputs = formElement.querySelectorAll('[name]:not([disabled])')
+                    var valueInputs = Array.from(enableInputs).reduce(function (value, input){
+                        value[input.name] = input.value
+                        return value
+                    }, {})
+                    options.onSubmit(valueInputs)
+                }
+            }
+        }
+
+        // event fill input
         options.rules.forEach(function (rule) {
             var inputElement = formElement.querySelector(rule.selector);
-            var errorMessage = inputElement.parentElement.querySelector(options.formMessage) 
+            var errorMessage = getParrent(inputElement, options.formGroupSelector).querySelector(options.formMessage) 
             // console.log(inputElement)
 
-            // save rule into the selectorRule
+            // save rule into the selectorRules
             if (Array.isArray(selectorRules[rule.selector])) {
                 selectorRules[rule.selector].push(rule)
             }
@@ -35,13 +76,14 @@ function Validator(options) {
                 selectorRules[rule.selector] = [rule]
             }
 
+            // event blur out of input
             if (inputElement) {
                 inputElement.onblur = function() {
-                   check(inputElement, rule)
+                   validate(inputElement, rule)
                 }
                 inputElement.onclick = function() {
                     errorMessage.innerText = ""
-                    inputElement.parentElement.classList.remove('invalid')
+                    getParrent(inputElement, options.formGroupSelector).classList.remove('invalid')
                 }
             }
         })
@@ -87,7 +129,7 @@ Validator.checkPassword = function(selector, getConfirm, message) {
         getConfirm: getConfirm,
         errorMessage: message || "Xác nhận mật khẩu không đúng.",
         test: function(pwCheck) {
-            return pwCheck === getConfirm() ? true : false;
+            return getConfirm() && pwCheck === getConfirm() ? true : false;
         }
     }
 }
